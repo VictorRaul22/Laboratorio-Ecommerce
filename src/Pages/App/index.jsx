27bf5d36fd45a639718fb361a780b0d7401f6578
Victor Chirinos Jimenez <victor-raul-22@hotmail.com>
2023-09-13 22:27:12 -0500

    proteccion de rutas

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,5 +1,5 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom'
-import { ShoppingCartProvider } from '../../Context'
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom'
+import { ShoppingCartContext, ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -10,22 +10,26 @@ import SignUp from '../SignUp'
 import Navbar from '../../Components/Navbar'
 import CheckoutSideMenu from '../../Components/CheckoutSideMenu'
 import './App.css'
+import { useContext } from 'react'
 
 const AppRoutes = () => {
+  const context=useContext(ShoppingCartContext);
+
   let routes = useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/clothes', element: <Home /> },
-    { path: '/electronics', element: <Home /> },
-    { path: '/furnitures', element: <Home /> },
-    { path: '/toys', element: <Home /> },
-    { path: '/othes', element: <Home /> },
-    { path: '/my-account', element: <MyAccount /> },
-    { path: '/my-order', element: <MyOrder /> },
-    { path: '/my-orders', element: <MyOrders /> },
-    { path: '/my-orders/last', element: <MyOrder /> },
-    { path: '/my-orders/:id', element: <MyOrder /> },
-    { path: '/sing-in', element: <SignIn /> },
-    { path: '/sing-up', element: <SignUp /> },
+    { path: '/', element:context.auth? <Home />: <Navigate to="/sing-in" replace={true} />},
+    { path: '/clothes', element:context.auth? <Home /> : <Navigate to="/sing-in" replace={true}/>},
+    { path: '/electronics', element:context.auth? <Home />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/furnitures', element:context.auth? <Home />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/toys', element:context.auth? <Home />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/othes', element:context.auth? <Home />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/my-account', element:context.auth? <MyAccount />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/my-order', element:context.auth? <MyOrder />: <Navigate to="/sing-in" replace={true}/> },
+    { path: '/my-orders', element:context.auth? <MyOrders /> : <Navigate to="/sing-in" replace={true}/>},
+    { path: '/my-orders/last', element:context.auth? <MyOrder /> : <Navigate to="/sing-in" replace={true}/>},
+    { path: '/my-orders/:id', element: context.auth?<MyOrder />: <Navigate to="/sing-in" replace={true}/> },
+
+    { path: '/sing-in', element:context.auth?<Navigate to="/" replace={true} /> : <SignIn />},
+    { path: '/sing-up', element: context.auth?<Navigate to="/" replace={true} /> : <SignUp /> },
     { path: '/*', element: <NotFound /> },
   ])
 
